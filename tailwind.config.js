abafd9f0a369fcf6b9e3691a824f9cc0a1bd8629
Arbha Pradana <arbhapr@gmail.com>
2022-07-24 23:02:37 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      screens: {
+        "2xl": "1320px",
+      },
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
