47e1bd61ad5a7781155f790b24d20f392907130d
Arbha Pradana <arbhapr@gmail.com>
2022-08-02 12:23:46 +0700

    reposition all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "index.html"],
+  content: ["./dist/**/*.{html,js}", "index.html"],
   theme: {
     container: {
       center: true,
